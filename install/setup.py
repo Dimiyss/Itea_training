00b1e81ca3e9696abf1a8b734bedf168c36084b5
dmytro.arkhypov <dmytro.arkhypov@pm.bet>
2021-11-06 21:24:56 +0200

    classes_5 and 6 and hw_5

---
+++ install/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Library_DA',
+    version='1.0.1',
+    description='Library was developed in ITEA Training for Home Work 5',
+    author='Dmytro Arkhypov',
+    packeges=['library_DA', 'library_DA.utils'],
+    instal_requires=['typing']
+)
---
+++ library_DA/utils/book.py
@@ -0,0 +1,27 @@
+class Book:
+    """Class that describe book in library"""
+    count = 0
+
+    def __init__(self, title, author, published_date, genre):
+        Book.count += 1
+        self.__id = Book.count
+        self.__title = title
+        self.__author = author
+        self.__published_date = published_date
+        self.__genre = genre
+        self.__current_place = 0  # 0 - library, else - reader id
+
+    def get_id(self) -> int:
+        return self.__id
+
+    def get_current_place(self):
+        return self.__current_place
+
+    def set_current_place(self, reader_id):
+        self.__current_place = reader_id
+
+    def __str__(self):
+        return f'{self.__id}:{self.__title}'
+
+    def __repr__(self):
+        return f'{self.__id}:{self.__title}'
